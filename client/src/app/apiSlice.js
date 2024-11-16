2ae3aad25f0f388854aa0798e7145e77d867d2c8
Es-Mes <esterleah085@gmail.com>
2024-11-17 00:06:17 +0200

    Uplouding the website!

--- client/src/app/apiSlice.js
+++ client/src/app/apiSlice.js
@@ -2,7 +2,8 @@ import{createApi,fetchBaseQuery}from "@reduxjs/toolkit/query/react"
 import {setToken} from "../fetures/auth/authSlice"
 
 const baseQuery=fetchBaseQuery({
-    baseUrl:"http://localhost:1234",
+   // baseUrl:"http://localhost:1234",
+   baseUrl: "https://primary-react-node-mongodb-project.onrender.com",
     credentials:"include",
     prepareHeaders:(headers,{getState})=>{
         const token=getState().auth.token
--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'http://localhost:5500'
+    'http://localhost:5500',
+    'https://pluma-for-babies.onrender.com'
 ]
 
 const corsOptions = {
